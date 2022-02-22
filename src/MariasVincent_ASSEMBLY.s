68d4ff5b10a34c3f734f0436627e70ece2009fb4
Vincent Marias <vmarias@mines.edu>
2022-02-22 15:04:24 -0700

    moved MathProcedures::sum into MariasVincent_ASSEMBLY.s

---
+++ src/MariasVincent_ASSEMBLY.s
@@ -0,0 +1,29 @@
+.text
+    .globl sum
+    sum:
+        addi sp, sp, -12      # adjust stack to make room for for 3 items
+        sw t0, 8(sp)          # save register t0 for use afterward
+        sw t1, 4(sp)          # save register t1 for use afterward
+        sw t2, 0(sp)          # save register t2 for use afterward
+
+        add a0, zero, zero    # result = 0
+        add t0, zero, zero    # i = 0
+        add t1, zero, zero    # offset = 0
+        add t2, zero, zero    # byte_temp = 0
+
+        sumLoop:
+            slli t2, t0, 2         # byte_temp = i * 4
+            add t1, a2, t2         # offset = arrAddr + byte_temp
+            lw t1, 0(t1)           # offset = *(arrAddr + byte_temp)
+
+            add a0, a0, t1         # result += offset
+
+            addi t0, t0, 1         # i++
+            bne t0, a3, sumLoop    # loop while i < arrSize
+
+        lw t2, 0(sp)          # restore register t2 for caller
+        lw t1, 4(sp)          # restore register t1 for caller
+        lw t0, 8(sp)          # restore register t0 for caller
+        addi sp, sp, 12       # adjust stack to delete 3 items
+
+        jalr zero, ra, 0      # return result
