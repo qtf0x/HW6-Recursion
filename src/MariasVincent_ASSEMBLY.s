37d9b97d8df96653a872d8adf303b71b4907e088
Vincent Marias <vmarias@mines.edu>
2022-02-22 17:43:50 -0700

    implemented binarySearch in RISC-V

--- src/MariasVincent_ASSEMBLY.s
+++ src/MariasVincent_ASSEMBLY.s
@@ -1,6 +1,10 @@
+.data
+    .align 2
+    sumPrompt: .string "Summation: "
+
 .text
     # Arguments
-    #     a2: address of array cotnaining integers to sum
+    #     a2: address of array containing integers to sum
     #     a3: number of elements in the array
     # Return values
     #     a0: the summation of the integers in the array
@@ -42,12 +46,57 @@
     #     a0: index of value
     .globl binarySearch
     binarySearch:
-        addi sp, sp, -8    # space for 2 words
-        sw ra, 4(sp)       # save return address
-        sw t0, 0(sp)       # save t0, used within
+        addi sp, sp, -16                 # space for 4 words
+        sw ra, 12(sp)                    # save return address
+        sw t0, 8(sp)                     # save t0, used within
+        sw t1, 4(sp)                     # save t1, used within
+
+        addi a7, zero, 4                 # load PrintString service
+        la a0, sumPrompt                 # a0 = &sumPrompt
+        ecall
+
+        sw a3, 0(sp)                     # need a3 after jal
+        sub a3, a4, a3                   # a3 = endIndex - startIndex
+        addi a3, a3, 1                   # a3++
+        jal ra, sum                      # call sum procedure
+        lw a3, 0(sp)                     # restore a3
+
+        addi a7, zero, 1                 # load PrintInt service
+        ecall
+
+        addi a7, zero, 11                # load PrintChar service
+        addi a0, zero, 10                # a0 = '\n'
+        ecall
+
+        add t0, a3, a4                   # t0 = startIndex + endIndex
+        addi t1, zero, 2                 # t1 = 2
+        div a0, t0, t1                   # a0 = t0 / 2 (a0 = midIndex)
+
+        slli t1, t0, 2                   # t1 = midIndex * 4
+        add t1, a2, t1                   # t1 = addAddr + (midIndex * 4)
+        lw t1, 0(t1)                     # t1 = *(arrAddr + (midIndex * 4))
+        beq a5, t1, binarySearch_done    # base case
+
+        blt a5, t1, recurseLeft          # recursive case left
+        bgt a5, t1, recurseRight         # recursive case right
+
+        addi a0, zero, -1                # a0 = -1
+        jal zero, binarySearch_done      # fail case
+
+        recurseLeft:
+            addi a4, a0, -1    # a4 = midIndex - 1
+            jal ra, binarySearch
+            jal zero, binarySearch_done
+
+        recurseRight:
+            addi a3, a0, 1    # a3 = midIndex + 1
+            jal ra, binarySearch
+            jal zero, binarySearch_done
 
         binarySearch_done:
-            lw t0, 0(sp)        # restore t0
-            lw ra, 4(sp)        # restore ra
-            addi sp, sp, 8      # pop the stack frame
+            lw t1, 4(sp)        # restore t1
+            lw t0, 8(sp)        # restore t0
+            lw ra, 12(sp)       # restore ra
+            addi sp, sp, 16     # pop the stack frame
+
             jalr zero, ra, 0    # return to the caller
